7d285e1057ffbb6bdb4fba42d4aaa87fde094cb4
Solomon Bekele <solubman28@gmail.com>
2025-10-09 09:23:53 +0400

    add staging and production docker yaml file

--- src/repositories/sequelize/config/sequelize-cli.config.js
+++ src/repositories/sequelize/config/sequelize-cli.config.js
@@ -9,17 +9,8 @@ export default {
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
-
-    // Use a different storage type. Default: sequelize
-    // migrationStorage: "json",
-
-    // Use a different file name. Default: sequelize-meta.json
-    // migrationStoragePath: "sequelizeMeta.json",
-
-    // Use a different table name. Default: SequelizeMeta
-    // migrationStorageTableName: "sequelize_meta",
   },
-  test: {
+ test: {
     username: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "root",
     database: process.env.DB_NAME || "user_db_test",
@@ -35,4 +26,12 @@ export default {
     port: process.env.DB_PORT,
     dialect: "mysql",
   },
-};
+  staging: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  },
+}
